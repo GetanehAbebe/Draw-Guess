cd701fdba1cb0e898f315581631bd29f075123af
Getaneh Abebe <abebegetaneh.ag@gmail.com>
2023-01-28 19:57:38 +0200

    Update git ignore

--- draw_and_guess/client/src/app/gamesSlice.ts
+++ draw_and_guess/client/src/app/gamesSlice.ts
@@ -1,21 +1,50 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { PayloadAction, createSlice } from '@reduxjs/toolkit';
 import { RootState } from './store';
 import { SOCKET_URL } from '../config';
-import io,{Socket} from 'socket.io-client';
+import io, { Socket } from 'socket.io-client';
+
 
 export interface SocketState {
-  rooms?:any,
-  username?:string | null,
-  userId?:string | null,
-  roomId?:string
+  rooms?: any,
+  username?: string | null,
+  userId?: string | null,
+  roomId?: string
   socket: Socket
+}
+
+interface User {
+  firstName: string,
+  lastName: string,
+  nickName?: string
+}
+
+interface RoomUser {
+  joinDate: string
+  members?: User[]
+
+}
+
+interface Room {
+  roomId: string,
+  messages: string,
+  createDate: Date | string,
+  members?: RoomUser[]
+}
+
+
+
+interface Message {
+  content: string,
+  author: string,
+  date: Date | string,
+  roomId: string
 }
 
 const socket = io(SOCKET_URL)
 const initialState: SocketState = {
   username: localStorage.getItem("username"),
   socket,
- rooms:{1:{name:'roomName2', messages:[{content:'hello', author:'getaneh', date:'today'}]}},
+  rooms: { 1: { name: 'roomName2', messages: [{ content: 'hello', author: 'getaneh', date: 'today' }] } },
 };
 
 
@@ -25,43 +54,44 @@ export const gamesSlice = createSlice({
   name: 'games',
   initialState,
   reducers: {
-    addMessage: (state:RootState,{roomId, message}:any) => {
-      state.rooms={...state.rooms,1:{name:'roomName1', messages:[{content:'hello', author:'getaneh', date:'today'}]}, 2:{name:'roomName1', messages:[{content:'hello', author:'getaneh', date:'today'}]}};
+    addMessage: (state, action: PayloadAction<Message>) => {
+      const { content, author, date, roomId } = action.payload
+        state.rooms[roomId].messages.push({ content, author, date })
     },
-    setRooms:(state:RootState,{payload})=>{      
-         state.rooms = payload
-         
+    setRooms: (state, action: PayloadAction<Room[]>) => {
+      state.rooms = action.payload
+
     },
-    setUsername:(state:RootState,{payload})=>{
-      if(payload){
+    setUsername: (state, { payload }) => {
+      if (payload) {
         state.username = payload
         localStorage.setItem("username", payload)
       }
     },
-    setUserId:(state:RootState,{payload})=>{
-      if(payload){
+    setUserId: (state, { payload }) => {
+      if (payload) {
         state.userId = payload
         localStorage.setItem("userId", payload)
       }
     },
-    setRoomId:(state:RootState,{payload})=>{
-      if(payload){
+    setRoomId: (state, { payload }) => {
+      if (payload) {
         state.roomId = payload
-        if(!state.rooms[payload].messages) {
+        if (!state.rooms[payload].messages) {
           state.rooms[payload].messages = []
         }
       }
     }
   }
-  
+
 });
 export const rooms = (state: RootState) => state.games.rooms;
-export const username = (state:RootState)=>state.games.username;
-export const userId = (state:RootState)=>state.games.userId;
-export const roomId = (state:RootState)=>state.games.roomId;
-export const socketContext = (state:RootState)=>state.games.socket;
+export const username = (state: RootState) => state.games.username;
+export const userId = (state: RootState) => state.games.userId;
+export const roomId = (state: RootState) => state.games.roomId;
+export const socketContext = (state: RootState) => state.games.socket;
 
-export const {addMessage, setRooms,setUsername, setUserId , setRoomId } = gamesSlice.actions;
+export const { addMessage, setRooms, setUsername, setUserId, setRoomId } = gamesSlice.actions;
 
 
 
